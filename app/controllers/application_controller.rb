42a8458810f809f58f109d39ff1b6422f2cd496d
Sergio Díaz <sendorf@gmail.com>
2019-09-18 00:06:37 +0200

    Added internaziolization and screenshots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@
 # App controller
 class ApplicationController < ActionController::Base
   before_action :check_categories
+  around_action :switch_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+ 
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
   def check_categories
     Connector::ChuckNorris.categories if Category.count.zero?
